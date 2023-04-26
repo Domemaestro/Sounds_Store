9b7455e2b7c5b852e2b89961e30a1017daec9dc7
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-26 17:54:46 +0530

    commit

--- app/src/main/java/com/example/soundsstore/MainActivity.kt
+++ app/src/main/java/com/example/soundsstore/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.soundsstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.soundsstore.ui.theme.SoundsStoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    IntroPage()
                 }
             }
         }
@@ -30,17 +45,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun IntroPage(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color(0xFFEFB8C8))) {
+        //background inside Box
+        Box(contentAlignment = Alignment.BottomStart, modifier = Modifier.fillMaxSize()) {
+            //context (This activity)
+            val mContext = LocalContext.current
+
+            //button inside
+            Button(onClick = {},
+                shape = CircleShape,
+            modifier = Modifier
+                .padding(start = 50.dp, bottom = 50.dp)
+                .size(60.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFCCC2DC)),
+                elevation = ButtonDefaults.buttonElevation(15.dp),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                val image = painterResource(id = R.drawable.next_arrow)
+                Image(painter = image, contentDescription = "next Activity")
+
+            }
+        }
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SoundsStoreTheme {
-        Greeting("Android")
+        IntroPage()
     }
 }
