e1531a9e09b32a8ab50e499a40a89d9b93ee975f
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-26 19:14:56 +0530

    commit

--- app/src/main/java/com/example/soundsstore/FrontPage.kt
+++ app/src/main/java/com/example/soundsstore/FrontPage.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,7 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.soundsstore.ui.theme.SoundsStoreTheme
 
 class FrontPage : ComponentActivity() {
@@ -38,8 +47,45 @@ fun MainPage(){
     //For Gradient Background
     val gradBack = Brush.verticalGradient(listOf(Color.White, Color(0xFFffdde1), Color(0xFFEFB8C8)))
 
-    Column(modifier = Modifier.fillMaxSize().background(gradBack)) {
-        //Main Section
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(gradBack)) {
+        //Main Section container of all
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 40.dp, start = 25.dp, bottom = 16.dp)) {
+            //top section
+            Text(text = buildString {
+                append("New\n")
+                append("\n")
+                append("arrivals")
+            }, fontSize = 45.sp,
+            fontWeight = FontWeight.ExtraBold,
+            )
+        }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
+            Button(onClick = {}, modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFffdde1)),
+            elevation = ButtonDefaults.buttonElevation(15.dp)) {
+                Text(text = "All", color = Color.Black)
+            }
+
+            Button(onClick = {}, elevation = ButtonDefaults.buttonElevation(15.dp),
+            colors = ButtonDefaults.buttonColors(Color(0xFFffdde1))) {
+                Text(text = "Headphones", color = Color.Black)
+            }
+
+            Button(onClick = {}, modifier = Modifier.padding(start = 20.dp),
+                elevation = ButtonDefaults.buttonElevation(15.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFffdde1))) {
+                Text(text = "Earphones", color = Color.Black)
+            }
+            
+        }
+
 
     }
 }
