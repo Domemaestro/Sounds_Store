735acaf5949ffd4df793dcabffc070d828f563ab
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-27 22:52:28 +0530

    commit

--- app/src/main/java/com/example/soundsstore/FrontPage.kt
+++ app/src/main/java/com/example/soundsstore/FrontPage.kt
@@ -3,6 +3,7 @@ package com.example.soundsstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -20,9 +22,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -96,6 +101,24 @@ fun MainPage(){
 
         Box(modifier = Modifier.padding(top = 20.dp)) {
             //Cardview set up in Grid (LazyGrid)
+            Card(modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)
+                .padding(start = 20.dp, end = 20.dp), elevation = CardDefaults.cardElevation(20.dp), shape = RoundedCornerShape(30.dp)
+            ) {
+                Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
+                    Box(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp), contentAlignment = Alignment.TopCenter) {
+                        val image = painterResource(id = R.drawable.airpodspro3rd)
+                        Image(painter = image, contentDescription = null, contentScale = ContentScale.Fit,
+                            modifier = Modifier.padding(top = 10.dp))
+
+
+                    }
+                }
+
+            }
 
         }
 
