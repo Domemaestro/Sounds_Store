3c2036635ba7772e8cffe7e03e90c59b30969d18
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-27 20:23:31 +0530

    commit

---
+++ app/src/main/java/com/example/soundsstore/DataModel/DataUse.kt
@@ -0,0 +1,5 @@
+package com.example.soundsstore.DataModel
+
+//excuse my naming sense
+
+data class DataUse(val ImageResourceId:Int,val StringResourceId:Int)
---
+++ app/src/main/java/com/example/soundsstore/DataSource/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.soundsstore.DataSource
+
+import com.example.soundsstore.DataModel.DataUse
+
+class DataSource {
+    fun loadFunction():List<DataUse>{
+        return listOf<DataUse>(
+
+        )
+    }
+}
