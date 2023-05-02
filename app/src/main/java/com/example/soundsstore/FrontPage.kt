985f331d32b2a28c48598f2d5d57da730ebc9008
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-05-02 16:59:15 +0530

    commit

--- app/src/main/java/com/example/soundsstore/FrontPage.kt
+++ app/src/main/java/com/example/soundsstore/FrontPage.kt
@@ -2,7 +2,7 @@ package com.example.soundsstore
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
